75549bb67139e8b1044dff109151278347145770
mim <mim@exante.eu>
2017-03-13 14:49:12 +0000

    limit.buy/stop.buy parameters

--- samples/sample_cpp_grid.R
+++ samples/sample_cpp_grid.R
@@ -5,38 +5,41 @@ library(grid)
 options(debug=T)
 
 cfg <- config(backtest) %>% modifyList(list(
-# no_cache = F,
-  roll_position = F # if T, then close position roll of ANY instrument (TODO: do it on real roll only). if F - roll position into next contract
+  no_cache = F, # всегда из базы
+  no_save = F, # не писать кэш на диск
+  roll_position = T # if T, then close position roll of ANY instrument (TODO: do it on real roll only). if F - roll position into next contract
   
 ))
 
-# init logging, see ~/rticks.log
+# init logging, see rticks.log
 init_spd_log(cfg)
 
 # period of backtest
 start <- as_datetime("2016-01-01")
-stop <- as_datetime("2017-03-03")
+stop  <- as_datetime("2017-03-03")
 
 params <- data_frame(
   # limits
-  buy           = 20.5, # buy when price <= buy only.  NA. +Inf = buy always.  -Inf = buy never
-  sell          = +Inf, # sell when price>=sell only
+  limit.buy     = 20.5,  # buy when price <= buy only.  NA. +Inf = buy always.  -Inf = buy never
+  stop.buy      = 20,    # FIXME: no buy lower than 18
+  
+  limit.sell    = +Inf,  # sell when price>=sell only
+  stop.sell     = NA,    # FIXME: no sell above 19
 
   pos           = 0,     # initial position
-  
   
   spread        = 0.25,  # take profit
   
   gamma.buy     = 1,       # size to buy on each mpi
   gamma.sell    = 1,       # size to sell on each mpi (number of contracts)
 
-  symbol=         "VIX.CBOE",   # exante prefix of contract series
+  symbol        = "VIX.CBOE",   # exante prefix of contract series
 
   active_contract = 6             # which month to trade
 )
 
 perfs <- params %>% backtest("gamma", start=start, stop=stop, config=cfg)
-results<-attr(perfs,"params")
+results <- attr(perfs,"params")
 
 perfs <- perfs %>% metrics.gamma(results) # calculate additional metrics
 
