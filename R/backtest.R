406c29d7b41c89130ac54b5d8da1b7a3265b9105
mim <mim@exante.eu>
2017-03-16 15:03:14 +0000

    log_perfs cleanup

--- R/backtest.R
+++ R/backtest.R
@@ -5,51 +5,43 @@
 #' 
 #' @export
 backtest.chunk <- function(data, params, algo, config) {
-  price <- 0.5*(head(data$bid,1)+head(data$ask,1))
-  end_price <- 0.5*(tail(data$bid,1)+tail(data$ask,1))
-  wlog("backtest.chunk >", data$datetime %>% head(1) %>% as_datetime() %>% strftime("%y-%m-%d %H:%M:%S"),
-       "..",data$datetime %>% tail(1) %>% as_datetime()%>% strftime("%y-%m-%d %H:%M:%S"),
-       "price",price,
-       "pos",params$pos,
-       "cash",params$cash,
-       "equity",params$pos*price*params$multiplier+params$cash)
-  #print(as.data.frame(config))
-  #browser()
+  log_perfs("backtest.chunk in", data, params, params, 0.5*(head(data$bid,1)+head(data$ask,1)))
+
   r <- bt_gamma(algo, data, params, config)
-  #browser()
+  
   if(length(r$perfs$datetime)==0) {
-    wlog("backtest.chunk < empty results")
-    warning(paste("empty results",as.character(data$datetime[1]),"..",as.character(data$datetime%>%tail(1))))
+    wlog("backtest.chunk EMPTY PERFS")
+    warning(paste("**** EMPTY PERFS ****",as.character(data$datetime[1]),"..",as.character(data$datetime%>%tail(1))))
     return (r);
   }
-  #browser()
   
   d <- data %>% select(datetime, virtual_id, bid, ask) %>% as_data_frame()
-  #browser()
   d <- d %>% transmute(datetime=datetime, symbol=virtual_id, price=0.5*(bid+ask))
-  d <- d %>% arrange(symbol, datetime) %>% as_data_frame()
+  #d <- d %>% arrange(symbol, datetime) %>% as_data_frame()
   d$datetime <- as_datetime(d$datetime) %>% trunc(config$freq) %>% as_date()
-  d <- d %>% group_by(symbol, datetime) %>% filter(row_number()==n()) # %>% mutate(metric="price") #%>% filter(datetime>=min(r$datetime) & datetime<=max(r$datetime))
+  d <- d %>% group_by(symbol, datetime) %>% filter(row_number()==n()) 
+
   r$perfs$datetime <- as_datetime(r$perfs$datetime) %>% trunc(config$freq) %>% as_date()
   r$perfs <- as_data_frame(r$perfs)
-  #browser()
   r$perfs <- r$perfs %>% spread(metric, value) %>% 
     inner_join(d, by=c("datetime","symbol")) %>%
     gather(metric, value, -datetime, -symbol) %>% 
     arrange(datetime)
   r$perfs$datetime <- as_date(r$perfs$datetime)
-  
-  end_price <- 0.5*(tail(data$bid,1)+tail(data$ask,1))
-  wlog("backtest.chunk <", data$datetime %>% head(1) %>% as_datetime() %>% strftime("%y-%m-%d %H:%M:%S"),
-       "..",data$datetime %>% tail(1) %>% as_datetime()%>% strftime("%y-%m-%d %H:%M:%S"),
-       "end_price",end_price,
-       "end_pos",r$pos,
-       "cash",r$cash,
-       "equity",r$pos*end_price*params$multiplier+r$cash)
-  
+  log_perfs("backtest.chunk out",data, r, params, 0.5*(tail(data$bid,1)+tail(data$ask,1)))  
   return(r)
 }
 
+log_perfs <- function(name, data, r, params, price) {
+  wlog(data$datetime %>% head(1) %>% as_datetime() %>% strftime("%y-%m-%d %H:%M:%S"),
+       "..",data$datetime %>% tail(1) %>% as_datetime()%>% strftime("%y-%m-%d %H:%M:%S"),
+       "price", price,
+       "pos", r$pos,
+       "cash", r$cash,
+       "qty_buy", r$qty_buy,
+       "qty_sell", r$qty_sell,
+       "pnl",r$pos*price*params$multiplier+r$cash)
+}
 #' backtest list of chunks
 #' 
 #' @export
@@ -58,10 +50,8 @@ backtest <- function(params, algo, start=NULL, stop=lubridate::now(), instrument
     instruments <- params$symbol
   }
 
-  ilog("backtest","start",as.character(start),"stop",as.character(stop),"\n")  
-  ilog("logging into ",config$log_path)
-  #file.remove(config$log_path)
-  
+  ilog("backtest","start",as.character(start),"stop",as.character(stop), "logging into ",config$log_path)
+
   instruments <- instruments %>% query_instruments()
   
   params <- as_data_frame(params) %>% 
@@ -82,17 +72,14 @@ backtest <- function(params, algo, start=NULL, stop=lubridate::now(), instrument
   params <- params %>% mutate(pos=0, cash=0, qty_buy=0, qty_sell=0)
   ilog("backtest algo", algo)
   for(chunk in data) {
-    #dlog(0, as.yaml(params))
     # open positions in the chunk
     if(nrow(chunk)==0) {
-      ilog("skipped empty chunk", as.character(chunk$start),"\n")
+      wlog("skipped empty chunk", as.character(chunk$start),"\n")
     }else {
       ch = head(chunk,1)
       params$cash <- params$cash - params$pos*0.5*(ch$bid+ch$ask)*params$multiplier # open the pos
-      #browser()
       r <- chunk %>% backtest.chunk(params, algo=algo, config=config)
       perfs <- perfs %>% bind_rows(r$perfs)
-      #browser()
       params$pos  <- r$pos
       params$cash <- r$cash
       params$qty_buy <- r$qty_buy
