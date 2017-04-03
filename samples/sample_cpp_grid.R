0123881b2993141619a9432d891ffe3a95c52146
mim <mim@exante.eu>
2017-04-03 11:33:26 +0000

    sample_cpp_grid

--- samples/sample_cpp_grid.R
+++ samples/sample_cpp_grid.R
@@ -8,7 +8,7 @@ cfg <- config(backtest) %>% modifyList(list(
   no_cache = T, # всегда из базы
   no_save = T, # не писать кэш на диск
   log_level = LOG$OFF,
-  log_stdout = LOG$INFO, 
+  log_stdout = LOG$WARN, 
   roll_position = F, # if T, then close position roll of ANY instrument (TODO: do it on real roll only). if F - roll position into next contract
   custom_roll = roll_day(day_of_month=1), # at 1st of the month, at least 1 month ahead of expiration  
   perfs_freq = as.numeric(days(1))
@@ -17,7 +17,7 @@ cfg <- config(backtest) %>% modifyList(list(
 # init logging, see rticks.log
 init_spd_log(cfg)
 
-start <- as_datetime("2016-01-01")
+start <- as_datetime("2015-01-01")
 stop  <- as_datetime("2016-03-01")
 
 params <- data_frame(
@@ -66,6 +66,6 @@ params <- bind_rows(params, data_frame(
   active_contract = 1#seq(2,3)             # which month to trade
 ))
 
-r <- params %>% backtest("gamma", start=start, stop=stop, config=cfg)
-
-#bt_report(r)
+r <- params %>% backtest("gamma", start=start, stop=stop, config=cfg) 
+r %>% bt_reports()
+# r %>% bt_view()
