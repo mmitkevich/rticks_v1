8323dc9e37cd0f52a28da334ec20eb21d3e47cb1
mim <mim@exante.eu>
2017-04-12 15:49:05 +0000

    commission subtracted from both pnl & rpnl

--- R/gamma.R
+++ R/gamma.R
@@ -3,15 +3,17 @@
 #' @export
 metrics.gamma <- function(env) {
   #params = attr(perfs, "params")
-  
+  #browser()
   qtys <- env$perfs %>% spread(metric, value) %>% as_data_frame()
   if(has_name(qtys, "commission"))
     qtys <- qtys %>% select(-commission)
   qtys <- qtys %>% inner_join(env$params %>% select(symbol, spread, multiplier, commission), by="symbol")
   qtys <- qtys %>% mutate(
-    rpnl = pmin(qty_buy, qty_sell)*spread*multiplier, 
+    rpnl = pmin(qty_buy, qty_sell) * spread * multiplier - commission, 
     commission = commission*(qty_buy+qty_sell),
-    pnl = pnl - commission) %>% 
+    pnl = pnl - commission,
+    pnl_high = pnl_high - commission,
+    pnl_low = pnl_low - commission) %>% 
     select(-spread,-multiplier)
   qtys<- qtys %>% gather(metric, value, -datetime, -symbol)
   qtys
