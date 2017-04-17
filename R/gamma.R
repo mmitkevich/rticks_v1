1b33f09fc50a3baa531820fa3623ae661255c024
mim <mim@exante.eu>
2017-04-17 15:20:30 +0000

    bt_reports(no_commission=T) mode

--- R/gamma.R
+++ R/gamma.R
@@ -1,7 +1,7 @@
 #' add metrics
 #' 
 #' @export
-metrics.gamma <- function(env) {
+metrics.gamma <- function(env, no_commission=F) {
   #params = attr(perfs, "params")
   #browser()
   qtys <- env$perfs %>% spread(metric, value) %>% as_data_frame()
@@ -9,12 +9,18 @@ metrics.gamma <- function(env) {
     qtys <- qtys %>% select(-commission)
   qtys <- qtys %>% inner_join(env$params %>% select(symbol, spread, multiplier, commission), by="symbol")
   qtys <- qtys %>% mutate(
-    rpnl = pmin(qty_buy, qty_sell) * spread * multiplier - commission, 
-    commission = commission*(qty_buy+qty_sell),
-    pnl = pnl - commission,
-    pnl_high = pnl_high - commission,
-    pnl_low = pnl_low - commission) %>% 
-    select(-spread,-multiplier)
-  qtys<- qtys %>% gather(metric, value, -datetime, -symbol)
+    rpnl = pmin(qty_buy, qty_sell) * spread * multiplier)
+  
+  if(!no_commission)
+    qtys <- qtys %>% mutate(
+      rpnl = rpnl - commission, 
+      commission = commission*(qty_buy+qty_sell),
+      pnl = pnl - commission,
+      pnl_high = pnl_high - commission,
+      pnl_low = pnl_low - commission)
+  else
+    qtys <- qtys %>% mutate(commission = 0)
+  
+  qtys<- qtys %>% select(-spread,-multiplier) %>% gather(metric, value, -datetime, -symbol)
   qtys
 }
