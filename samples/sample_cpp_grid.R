42f5d161717602660eca21159ba48048281cb68e
mim <mim@exante.eu>
2017-04-20 12:57:06 +0000

    sample with bt_summaries

--- samples/sample_cpp_grid.R
+++ samples/sample_cpp_grid.R
@@ -7,12 +7,12 @@ options(debug=T)
 cfg <- config(backtest) %>% modifyList(list(
   no_cache = T, # всегда из базы
   no_save = F, # не писать кэш на диск
-  log_level = LOG$OFF,
+  log_level = LOG$DEBUG,
   log_stdout = LOG$WARN, 
   zero_position_on_rolls = F,
   zero_position_freq = F, #as.numeric(months(2)), 
   custom_roll = roll_day(day_of_month=1), # at 1st of the month, at least 1 month ahead of expiration  
-  perfs_freq = as.numeric(days(1)),
+  perfs_freq = as.numeric(minutes(1)),
   perfs_tz = as.integer(16)
 ))
 
@@ -26,7 +26,6 @@ params <- data_frame(
   # limits
   
   limit.buy     = -200,    # buy when price <= buy only.  NA. +Inf = buy always.  -Inf = buy never
-  stop.buy      = -360,    # FIXME: no buy lower than 18
   
   limit.sell    = +Inf,  # sell when price>=sell only
   stop.sell     = NA,    # FIXME: no sell above 19
@@ -74,4 +73,8 @@ params <- bind_rows(params, data_frame(
 r <- params %>% backtest("gamma", start=start, stop=stop, config=cfg) 
 bt_reports(r)
 #bt_view_metrics(r, start="2015-09-23 15:48:00", stop="2015-09-25")
+#r$metrics %>% filter(pos!=lag(pos) | pos!=lead(pos)|bid!=lag(bid)|ask!=lag(ask)|bid!=lead(bid)|ask!=lead(ask)) %>% filter_date("2016-01-01","2016-05-01") %>% View()
 bt_plot(r,no_gaps=F)
+print(bt_summaries(r))
+
+#r$metrics %>% filter_date("2015-12-18") %>% filter(bid!=lead(bid)|ask!=lead(ask)|pos!=lead(pos)) %>% View()
