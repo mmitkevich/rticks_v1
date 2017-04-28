09b15ea5e63f0f513e244e6cc4abd1eba58c2cde
mim <mim@exante.eu>
2017-04-28 10:13:26 +0000

    fixed drisk

--- R/gamma.R
+++ R/gamma.R
@@ -20,9 +20,10 @@ metrics.gamma <- function(env, no_commission=F) {
       pnl_low = pnl_low - commission)
   else
     qtys <- qtys %>% mutate(commission = 0)
+  p <- env$params
   qtys <- qtys %>% mutate(      
-    drisk = (lag(pos)+0.5*(lag(price)-env$params$risk.buy)/env$params$mpi*env$params$gamma.buy)*(lag(price)-env$params$risk.buy),
-    rtn = (pnl-lag(pnl))/drisk)
+    drisk = (pos+0.5*(min(price,p$limit.buy)-p$stop.buy)/p$mpi*p$gamma.buy)*(price-p$risk.buy),
+    rtn = (pnl-lag(pnl))/lag(drisk))
   qtys<- qtys %>% select(-spread,-multiplier) %>% gather(metric, value, -datetime, -symbol)
   qtys
 }
