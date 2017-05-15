338ac7b8d8b1426ff480c99b77097c0ff00319af
mim <mim@exante.eu>
2017-05-15 16:08:55 +0000

    changed formula drisk

--- R/gamma.R
+++ R/gamma.R
@@ -1,7 +1,7 @@
 #' add metrics
 #' 
 #' @export
-metrics.gamma <- function(env, no_commission=F) {
+metrics.gamma <- function(env, no_commission=F, currency=NULL) {
   #params = attr(perfs, "params")
   #browser()
   qtys <- env$perfs %>% spread(metric, value) %>% as_data_frame()
@@ -22,7 +22,8 @@ metrics.gamma <- function(env, no_commission=F) {
     qtys <- qtys %>% mutate(commission = 0)
   p <- env$params
   qtys <- qtys %>% mutate(      
-    drisk = (pos+0.5*(min(price,p$limit.buy)-p$stop.buy)/p$mpi*p$gamma.buy)*(price-p$risk.buy),
+    #drisk = (pos+0.5*(min(price,p$limit.buy)-p$stop.buy)/p$mpi*p$gamma.buy)*(price-p$risk.buy),
+    drisk = (((min(price,p$limit.buy)-p$stop.buy)/p$mpi+1)*((min(price,p$limit.buy)-p$stop.buy)/2*p$gamma.buy+(p$stop.buy-p$risk.buy))+pos*(price-p$risk.buy))*p$multiplier,
     rtn = (pnl-lag(pnl))/lag(drisk))
   qtys<- qtys %>% select(-spread,-multiplier) %>% gather(metric, value, -datetime, -symbol)
   qtys
