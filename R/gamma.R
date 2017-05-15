68383dd16cad642058dba5be88a67f2e4afc735d
Karlis Liepins <kl@exante.eu>
2017-05-15 12:44:28 +0000

    corrected drisk formula

--- R/gamma.R
+++ R/gamma.R
@@ -22,7 +22,7 @@ metrics.gamma <- function(env, no_commission=F) {
     qtys <- qtys %>% mutate(commission = 0)
   p <- env$params
   qtys <- qtys %>% mutate(      
-    drisk = (((min(price,p$limit.buy)-p$stop.buy)/p$mpi+1)*((min(price,p$limit.buy)-p$stop.buy)/2*p$gamma.buy+(p$stop.buy-p$risk.buy))+pos*(price-p$risk.buy))*p$multiplier,
+    drisk = (((min(price,p$limit.buy)-min(p$stop.buy,bid))/p$mpi+1)*((min(price,p$limit.buy)-min(p$stop.buy,bid))/2*p$gamma.buy+(p$stop.buy-p$risk.buy))+pos*(price-min(p$risk.buy,price)))*p$multiplier,
     rtn = (pnl-lag(pnl))/lag(drisk))
   qtys<- qtys %>% select(-spread,-multiplier) %>% gather(metric, value, -datetime, -symbol)
   qtys
