a21f3e680d2a06a11dcb81342f9314fa444f4e35
mim <mim@exante.eu>
2017-05-15 16:08:29 +0000

    code style

--- R/clean_chunk.R
+++ R/clean_chunk.R
@@ -17,11 +17,15 @@ load_trade_schedule <- function(instrument_id,
   ticker <- q %>% map_chr(~ .x[1])
   exchange <- q %>% map_chr(~ .x[2])
   future_part <- q %>% map_chr(~ .x[3])
-  exanteID <- ifelse(substr(future_part, 0, 2) %in% c("RS", "CS"), paste0(ticker, ".", exchange, ".", substr(future_part, 0, 2),"/X0000-X0000^"), paste0(instrument_id,".X0000^"))
+  exanteID <- ifelse(substr(future_part, 0, 2) %in% c("RS", "CS"), 
+                     paste0(ticker, ".", 
+                            exchange, ".", 
+                            substr(future_part, 0, 2),"/X0000-X0000^"), 
+                     paste0(instrument_id,".X0000^"))
   mappingFile <- fromJSON(paste0(path,".mapping"))
-  fileName <- head(mappingFile$mapping$schedule[sapply(mappingFile$mapping$match, function(x) grepl(x, exanteID, perl = T))],1)
+  fileName <- head(mappingFile$mapping$schedule[sapply(mappingFile$mapping$match, function(x) grepl(x, exanteID, perl = T))], 1)
   fullFileName <- paste0(path, fileName, ".json")
-  wlog("load_trade_schedule from",fullFileName)
+  wlog("load_trade_schedule from", fullFileName)
   rawHistoricalSchedule <- fromJSON(fullFileName)
   startDateTime <- ifnull(start, as_datetime(min(rawHistoricalSchedule$intervals$start)))
   endDateTime <-  ifnull(end, as_datetime(now()))
