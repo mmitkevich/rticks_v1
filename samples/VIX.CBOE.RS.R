f79ef5655a60204bb37a679f640c226c273dfaf2
mim <mim@exante.eu>
2017-06-08 16:54:03 +0000

    samples/VIX.CBOE.RS.R

---
+++ samples/VIX.CBOE.RS.R
@@ -0,0 +1,50 @@
+library(rticks)
+library(ggplot2)
+
+options(debug=T)
+
+cfg <- config(backtest) %>% modifyList(list(
+  no_cache = T, # всегда из базы
+  no_save = T, # не писать кэш на диск
+  log_level = LOG$INFO,
+  log_stdout = LOG$WARN,
+  zero_position_freq= F, #as.numeric(months(2)),
+  zero_position_on_rolls = F,
+  custom_roll = roll_day(day_of_month=1), # at 1st of the month, months_ahead=1 at least 1 month ahead of expiration  
+  perfs_freq = as.numeric(days(1)),
+  perfs_tz = as.integer(16),
+  roll_same_day_all_legs=T
+))
+
+# init logging, see rticks.log
+init_spd_log(cfg)
+
+# period of backtest
+start <- as_datetime("2015-01-01")
+stop  <- as_datetime("2017-06-01")
+
+params <- data_frame(
+  # limits
+  limit.buy     = 0.35,  # buy when price <= buy only.  NA. +Inf = buy always.  -Inf = buy never
+  stop.buy      = 0.0,    # FIXME: no buy lower than 18
+  risk.buy      = -1.0,
+  limit.sell    = +Inf,  # sell when price>=sell only
+  stop.sell     = NA,    # FIXME: no sell above 19
+  
+  pos           = 0,     # initial position
+  
+  spread        = 0.05,  # take profit
+  
+  gamma.buy     = 1,       # size to buy on each mpi
+  gamma.sell    = 1,       # size to sell on each mpi (number of contracts)
+  
+  symbol        = "VIX.CBOE.RS1M",   # exante prefix of contract series
+  
+  #roll_pattern  = list(list(12)),
+  min_active_contract = 5,
+  active_contract = 5             # which month to trade
+)
+
+r <- params %>% backtest("gamma", start=start, stop=stop, config=cfg)
+bt_reports(r, save = F)
+bt_plot(r, maxpoints = 1000)
