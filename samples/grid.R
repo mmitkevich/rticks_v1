7e1739c0adf5cf0c146453e12fb419d7ca2a6eb6
mim <mim@exante.eu>
2017-08-21 09:22:49 +0000

    grid_test.R

--- samples/grid.R
+++ samples/grid.R
@@ -4,22 +4,11 @@ library(grid)
 
 options(debug=T)
 options(error = recover)
-
 cfg.reload()
-
 parinit(10)
+bt <- config.gamma("samples/grid_test.yaml")
 
-bt <- config.gamma("samples/grid_bt.yaml") %>%   run_all.gamma(enabled="PL-GC", run_name = run_name_today("PL-GC-WF"), keep_data = F,IIS = c(1,3,10,20))
-
-
-#all_params <- bt$results %>% group_by(symbol) %>% filter(row_number()==1) %>% select(-weight)
-#par <- all_params[1,] %>% as_data_frame()
-#par$symbol<-str_left(par$symbol,2)[[1]]
-#wfrun <- par %>% backtest(stparams=par, "gamma", start=bt$config$start, stop=bt$config$stop, config=bt$config) 
-
-#run_name <- "20170809/092840-CL-WF-YZNHU"
-#bt <- bt %>% walk_forward.gamma(run_name = run_name, IIS = c(3))
-
+bt <- bt %>% run_all.gamma(enabled="PL-GC", run_name = run_name_today("PL-GC-WF"), keep_data = F, IIS = c(1,3,10,20))
 
 parstop()
 last_plot()
