d24d07b83478ed9e9a8d96ac4ad55c37c18e4e7c
mim <mim@exante.eu>
2017-08-23 13:32:28 +0000

    C.ICE sample

--- samples/grid.R
+++ samples/grid.R
@@ -8,7 +8,7 @@ cfg.reload()
 parinit(0)
 bt <- config.gamma("samples/grid_test.yaml")
 
-bt <- bt %>% run_all.gamma(enabled="PL-GC", run_name = run_name_today("PL-GC-WF"), keep_data = F, IIS = c(1,3,10,20))
+bt <- bt %>% run_all.gamma(enabled="C", run_name = run_name_today("PL-GC-WF"), keep_data = F, IIS = c(1,3,10,20))
 
 parstop()
 last_plot()
