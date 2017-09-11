2b1cd33ab809de28176303c3c3e907e531830574
mim <mim@exante.eu>
2017-09-11 11:42:16 +0000

    BRUB setup

--- samples/grid.R
+++ samples/grid.R
@@ -7,6 +7,7 @@ options(debug=F)
 cfg.reload()
 parinit(8)
 bt <- config.gamma("samples/grid_bt.yaml")
-bt <- bt %>% run_all.gamma(enabled=c("C","BRUB","PL-GC"), run_name = run_name_today("C-BRUB-PLGC-test-WF"), keep_data = F, IIS = c(1,3,10,20))
+MINS_PER_DAY=7*60*60
+bt <- bt %>% run_all.gamma(enabled=c("BRUB"), run_name = run_name_today("BRUB-min-test-WF"), keep_data = F, IIS = c(3*MINS_PER_DAY))
 parstop()
 last_plot()
