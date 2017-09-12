613a0b49ecba15bb915037e5d9a1da66e2f70b4f
mim <mim@exante.eu>
2017-09-12 14:52:38 +0000

    cleaned grid.R for 8-cores

--- samples/grid.R
+++ samples/grid.R
@@ -5,13 +5,12 @@ library(grid)
 options(debug=F)
 #options(error = recover)
 cfg.reload()
-parinit(0)
-bt <- config.gamma("samples/grid_po.yaml")
+parinit(8)
+bt <- config.gamma("samples/grid_bt.yaml")
 bt <- bt %>% run_all.gamma(
-  enabled = c("LH"), 
-  run_name = run_name_today("LH-test"), 
-  keep_data = F, 
-  IIS = c(1, 5, 10, 100))
+#  enabled = c("LH","C","CC"), 
+  run_name = run_name_today("WF-ALL-tmp"), 
+  IIS = c(1, 5, 10, 30, 60, 90))
 parstop()
 last_plot()
 bt_plot(bt$runs[[1]])
