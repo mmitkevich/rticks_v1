b11b874bc22e67213c6f8a54169d8e16d1d7550c
mim <mim@exante.eu>
2017-10-12 15:03:28 +0000

    grid_bt.R

--- samples/grid.R
+++ samples/grid.R
@@ -2,18 +2,17 @@ library(rticks)
 library(ggplot2)
 library(grid)
 
-options(debug=F)
+options(debug=F, cluster=4)
 #options(error = recover)
 cfg.reload()
-parinit(0)
-bt <- config.gamma("samples/grid_test_results.yaml")
+#parinit(20)
+bt <- config.gamma("samples/grid_bt.yaml")
 bt <- bt %>% run_all.gamma(
-  enabled = c("C"), 
-  run_name = run_name_today("WF-ALL-rpnl")
+#  enabled = c("CL"), 
+  run_name = run_name_today("WF-ALL-max")
   #IIS = c(30, 60, 90), 
   #OOS = c(1,   5, 30)
 )
-parstop()
+#parstop()
 last_plot()
 bt_plot(bt$runs[[1]])
-#
