128404eb21cb177cb6aef5ac1d4edc5d60c0d5a4
mim <mim@exante.eu>
2017-10-13 11:59:37 +0000

    grid_wf.yaml

--- samples/grid.R
+++ samples/grid.R
@@ -5,10 +5,10 @@ library(grid)
 options(debug=F, cluster=4)
 #options(error = recover)
 cfg.reload()
-bt <- config.gamma("samples/grid.yaml")
+bt <- config.gamma("samples/grid_wf.yaml")
 bt <- bt %>% run_all.gamma(
 #  enabled = c("C", "CL", "CC"), 
-  run_name = run_name_today("KL-WF-test")
+  run_name = run_name_today("MiM-WF-ALL-MAX-RPNL")
 )
 last_plot()
 bt_plot(bt$runs[[1]])
